505a8c8b3893bcfe5d440ef470184b7d93a808f1
Bruce Van Horn <bvanhorn@cleartechnologies.net>
2020-04-20 16:32:38 -0500

    my hands are typing words

--- KitchenSinkConsole/Program.cs
+++ KitchenSinkConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Threading;
 
@@ -93,11 +94,66 @@ namespace KitchenSinkConsole
             concat += "\nYou're going to have to do some work which can be error prone.";
 
             var stringBuilder = new StringBuilder("The price of a sink made by " + brandName + " is " + price);
-            stringBuilder.Append(". Which is even easier and cleaner with string builders.");
+            stringBuilder.Append(". ");
+            stringBuilder.Append("Which is even easier and cleaner with string builders.");
             stringBuilder.AppendLine("Here if you want a new line, just append it that way.");
             stringBuilder.AppendLine("Generally, this code is easier to understand and maintain.");
 
+            /*  2.8  String Formatters
+             *
+             *  Another convenience feature in C# is a string formatter.
+             *  Just about every language has these.  They are meant to take
+             *  the tedium out of concatenation.
+             *
+             */
 
+            var pricePerPound = 23.33m;
+            var strPricePerPound = string.Format("The current price is {0:C2} per pound.", pricePerPound);
+            Console.WriteLine(strPricePerPound);
+
+            //you can do multiples, too.
+            var timeAndTemperature = String.Format("At {0}, the temperature is {1}°F.",
+                DateTime.Now, 20.4);
+            Console.WriteLine(timeAndTemperature);
+
+            /*  2.9  Interpolated & Verbatim Strings
+             *
+             *   Most languages have strings that feature escape characters.
+             *   For example \n means "new line" and \t means "tab".  But sometimes
+             *   you just want your strings to behave exactly how you type them.
+             */
+
+            var inventorName = "Jim Schmendrick";
+            var rightNow = DateTime.Now;
+
+            // There are two ways to use interpolated strings.
+            // First, composite formatting
+            Console.WriteLine("Did you know the kitchen sink was invented by {0}", inventorName);
+
+            //  This looks the same as the string formatter example except that it
+            //  lacks the string.formatter statement.
+
+            //The other way is to use $ to put expressions directly in the code.
+            Console.WriteLine($"It is currently {rightNow.DayOfWeek} and the current time is {rightNow:HH:mm}");
+            var thisFile = new FileInfo(@"c:\temp\test.txt");
+
+            //  If you find yourself want to to use $ and @ together, you can
+            //  easily in C# 8, but in earlier version it had to be in a
+            //  particular order.
+            
+            var triangleBase = 16;
+            var triangleHeight = 32;
+            var triangleArea = (triangleBase * triangleHeight) / 2;
+
+            // Here, $ token appears before @ token 
+            Console.WriteLine("Finding the area of a triangle:");
+            Console.WriteLine($@"Height = ""{triangleHeight}"" and Base = ""{triangleBase}""");
+            Console.WriteLine($@"Area = ""{triangleArea}""");
+
+            //as of C# 8, the order no longer matters
+            Console.WriteLine("Finding the area of a triangle AGAIN:");
+            Console.WriteLine(@$"Height = ""{triangleHeight}"" and Base = ""{triangleBase}""");
+            Console.WriteLine(@$"Area = ""{triangleArea}""");
         }
     }
 }
